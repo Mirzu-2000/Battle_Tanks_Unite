8d4fc01c3fa381fb17864187a5cfc069de63d04f
Mirza_4565 <mirzaalishaik2000@gmail.com>
2025-06-15 11:58:47 +0530

    Tank Selection UI

---
+++ Assets/Scripts/Tank/TankSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Tank
+    {
+        public float movementSpeed;
+
+        public float rotationSpeed;
+
+        public TankTypes tankType;
+
+        public Material color;
+
+
+    }
+
+    public List<Tank> tanklist;
+
+   public TankView tankView;
+    void Start()
+    {
+       
+    }
+
+    public void CreateTank(TankTypes tankType)
+    {
+        if (tankType == TankTypes.GreenTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[0].movementSpeed, tanklist[0].rotationSpeed, tanklist[0].tankType, tanklist[0].color);
+
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+
+       else if (tankType == TankTypes.BlueTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[1].movementSpeed, tanklist[1].rotationSpeed, tanklist[1].tankType, tanklist[1].color);
+
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+
+        else if (tankType == TankTypes.RedTank)
+        {
+            TankModel tankModel = new TankModel(tanklist[2].movementSpeed, tanklist[2].rotationSpeed, tanklist[2].tankType, tanklist[2].color);
+
+            TankController tankController = new TankController(tankModel, tankView);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/UI/TankSelection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+
+    public TankSpawner tankSpawner;
+   public void BlueTankSelected()
+   {
+        tankSpawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+
+
+        Debug.Log("BLUE");
+   }
+
+
+    public void GreenTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+
+        Debug.Log("Green");
+    }
+
+    public void RedTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+
+        Debug.Log("Red");
+    }
+}
